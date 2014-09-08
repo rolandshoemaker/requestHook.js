c73e3e6af22c8874388f72df3bd1297ca06a264a
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-08 00:29:19 -0700

    fixed it up a bit

---
+++ README.md
@@ -0,0 +1,11 @@
+requestHook.js
+==============
+
+a very simple javascript file that will hook jquery's $.ajax() function allowing all calls to $.ajax(), $.get(), and $.post() to be cloned to another address.
+
+This was originally written to be used with [postMonitor](https://github.com/rolandshoemaker/postMonitor).
+
+Example
+-------
+
+uuuum. Load it **after** jQuery. That's about it. oh and define clone_addr!
--- requestHook.js
+++ requestHook.js
@@ -1,22 +1,21 @@
 // requestHook.js - roland shoemaker 2014
-// i guess this could be a little evil...
+// hooks $.ajax and by inheritence $.get and $.post, i guess this could be a little evil...
 
-clone_addr = "127.0.0.l:8080"
-
-getClone = $.get
-$.get = function(e, r, i, o) {
-	var parser = document.createElement('a');
-	parser.href = e	
-	getClone('http://'+clone_addr+parser.pathname+parser.search+parser.hash, r, i ,o);
-	return getClone(e, r, i, o);
-}
+clone_addr = "192.168.1.130:8080";
 
 ajaxClone = $.ajax
-$.ajax = function(e, n) {
+$.ajax = function (e, n) {
 	var parser = document.createElement('a');
-	parser.href = e
-	ajaxClone('http://'+clone_addr+parser.pathname+parser.search+parser.hash, n);
+	if (typeof(e) == "object") {
+		#var copy = jQuery.extend(true, {}, e)
+		var copy = JSON.parse(JSON.stringify(e))
+		parser.href = e.url;
+		copy.url = 'http://'+clone_addr+parser.pathname+parser.search+parser.hash
+		ajaxClone(copy, n);
+	}
 	return ajaxClone(e, n);
 }
+
+
 
 
